b135e6c65dafa226983db185819ac11694336106
Yves Senn <yves.senn@gmail.com>
2011-03-05 16:01:50 +0100

    simplified adapter creation

--- lib/backyard/configuration.rb
+++ lib/backyard/configuration.rb
@@ -32,14 +32,18 @@ module Backyard
     end
 
     def adapter_instance
-      return @adapter_instance if @adapter_instance
-      adapter_class = adapter.to_s.split('_').map!{ |w| w.capitalize }.join
-      adapter_class_with_modules = "Backyard::Adapter::#{adapter_class}"
-      unless defined?(adapter_class_with_modules)
+      @adapter_instance ||= adapter_klass.new
+    end
+
+    def adapter_klass
+      adapter_name = adapter.to_s.split('_').map!{ |w| w.capitalize }.join
+      adapter_class = "Backyard::Adapter::#{adapter_name}"
+      unless defined?(adapter_class)
         require File.join('backyard', 'adapter', "#{adapter}")
       end
-      @adapter_instance = eval "#{adapter_class_with_modules}.new"
+      eval adapter_class
     end
+    private :adapter_klass
 
     class Model
       attr_writer :name_attributes, :name_blocks
